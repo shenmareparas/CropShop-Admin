d2cff674b408b36dec0d0217f2a58ef2524896ed
Paras Shenmare <shenmareparas@gmail.com>
2024-05-01 20:49:59 +0530

    fresh commit

---
+++ lib/services/assets_manager.dart
@@ -0,0 +1,20 @@
+class AssetsManager {
+  static String imagesPath = "assets/images";
+  static String categoriesImagesPath = "assets/images/categories";
+  static String dashboardImagesPath = "assets/images/dashboard";
+
+// general images
+  static String warning = "$imagesPath/warning.png";
+  static String error = "$imagesPath/error.png";
+  static String emptySearch = "$imagesPath/empty_search.png";
+  static String shoppingCart = "$imagesPath/shopping_cart.png";
+
+  // Categories path
+  static String vegetables = "$categoriesImagesPath/vegetables.png";
+  static String fruits = "$categoriesImagesPath/fruits.png";
+  static String dairy = "$categoriesImagesPath/dairy.png";
+  static String seeds = "$categoriesImagesPath/seeds.png";
+
+  static String order = "$dashboardImagesPath/order.png";
+  static String cloud = "$dashboardImagesPath/cloud.png";
+}
